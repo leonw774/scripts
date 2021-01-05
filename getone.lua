118035b630cb1e188557e68383096230617d0a74
PatrikLundell <j.patrik.r.lundell@gmail.com>
2021-01-05 11:19:52 +0100

    Add files via upload

--- getone.lua
+++ getone.lua
@@ -1,9 +1,58 @@
-local number_to_get = 1  --  Change this number if you want more of each. Note that the number of plants
-                         --  actually collected is reduced by the number of seeds already at hand.
+--  This script designates surface plants for collection, with the primary aim to get seeds for
+--  farm production. Plants already designated are taken into account.
+--  The script tries to select the plants closest to the center of the embark, but does not try
+--  to replace ones already designated with closer ones.
+--
+local get_all = true       --  If true, every shrub is collected and the flags below are ignored
+local get_paper = true     --    If true, include paper makeing plants (only papyrus in vanilla)
+local get_drink = true     --    If true, include booze producing plants
+local get_thread = true    --    If true, include thread producing plants
+local get_mill = true      --    If true, include millable plants
+local get_seedless = false -- Get plants that can't be grown as they produce no seeds. The vanilla
+                           --  plants of this type are Valley Herb and Kobold Bulb. Note that get_all
+                           --  does NOT override this setting.
+local number_to_get = 1    --  Change this number if you want more of each. Note that the number of plants
+                           --  actually collected is reduced by the number of seeds already at hand.
+local verbose = false      --  If true, prints a line for every designated plant and plants for which
+                           --  we already have sufficient seeds. If false, those messages are suppressed.
+local skip_evil = false    --  If set, ignores plants in an evil biome. The main purpose for this is to
+                           --  allow you to keep out of evil rain/clouds on multi biome embarks. If the
+                           --  plant doesn't exist in the rest of the embark none will be designated.
   
+local is_evil_offset = {}
+local count = 0
+
+for i = -1, 1 do
+  local x = df.global.world.world_data.region_details [0].pos.x + i
+  
+  if x < 0 then
+    x = 0
+  elseif x == df.global.world.world_data.world_width then
+    x = df.global.world.world_data.world_width - 1
+  end
+  
+  for k = -1, 1 do
+    local y = df.global.world.world_data.region_details [0].pos.y + k
+    
+    if y < 0 then
+      y = 0
+    elseif y == df.global.world.world_data.world_height then
+      y = df.global.world.world_data.world_height - 1
+    end
+    
+    is_evil_offset [1 + i + (k + 1) * 3] = df.global.world.world_data.region_map [x]:_displace (y).evilness >= 66
+  end
+end
+
 --====================================
 
-local max_x, max_y, max_z = dfhack.maps.getTileSize()
+function reject_evil (block, x, y)
+  return skip_evil and is_evil_offset [block.region_offset [block.designation [x] [y].biome]]
+end
+
+--====================================
+
+local max_x, max_y, max_z = dfhack.maps.getTileSize ()
 local center_x = max_x / 2
 local center_y = max_y / 2 
   
@@ -18,13 +67,16 @@ end
 --====================================
 
 function closer (x1, y1, x2, y2)
+  --  The relation of the square of the distances is the same as that of the distances, so it's
+  --  redundant to take the square root to compare the actual distances.
+  --
   return distance_2 (x1, y1) < distance_2 (x2, y2)
 end
 
 --====================================
 
 function getone ()
-  for i, v in ipairs (df.global.world.raws.plants.all) do
+  for i, v in ipairs (df.global.world.raws.plants.bushes) do
     local paper_plant = false
     
     for k, material in ipairs (v.material) do
@@ -36,12 +88,17 @@ function getone ()
       end
     end    
     
-    if (v.flags.DRINK or
-        v.flags.THREAD or
-        paper_plant) and
-       not v.flags.SAPLING and
-       not v.flags.TREE and
-       not v.flags.GRASS and
+    if (((get_all or
+         (get_drink and v.flags.DRINK) or
+         (get_thread and v.flags.THREAD) or
+         (get_mill and v.flags.MILL) or
+         (get_paper and paper_plant)) and
+         v.flags.SEED) or
+        not v.flags.SEED and
+        get_seedless) and
+       not v.flags.SAPLING and  --  Should be redundant as we iterate over bushes only
+       not v.flags.TREE and     --  -"-
+       not v.flags.GRASS and    --  -"-
        not v.flags.BIOME_SUBTERRANEAN_WATER and
        not v.flags.BIOME_SUBTERRANEAN_CHASM and
        not v.flags.BIOME_SUBTERRANEAN_LAVA then
@@ -51,12 +108,16 @@ function getone ()
       local skip = false
  
       for k, seed in ipairs (df.global.world.items.other.SEEDS) do
-        if seed.mat_index == i then
+        if seed.mat_index == v.index then
           seeds = seeds + 1
           
           if seeds >= number_to_get then
             skip = true
-            dfhack.println ("Skipping " .. v.id .. " as sufficient seeds are already available")
+            if verbose then
+              dfhack.color (COLOR_LIGHTGREEN)
+              dfhack.println ("Skipping " .. v.id .. " as sufficient seeds are already available")
+              dfhack.color (COLOR_RESET)
+            end
             break
           end
         end
@@ -67,13 +128,13 @@ function getone ()
       
       if not skip then
         for k, plant in ipairs (df.global.world.plants.all) do
-          if plant.material == i then
+          if plant.material == v.index then
             local cur = dfhack.maps.getTileBlock (plant.pos)
             local x = plant.pos.x % 16
             local y = plant.pos.y % 16
             local skip_inner
    
-            if cur.tiletype [x] [y] == df.tiletype.Shrub then
+            if not reject_evil (cur, x, y) and cur.tiletype [x] [y] == df.tiletype.Shrub then
               if cur.designation [x] [y].dig == df.tile_dig_designation.No then
                 current = df.global.world.jobs.list
                 skip_inner = false
@@ -144,7 +205,9 @@ function getone ()
           if growth.id == "FRUIT" and
              (df.global.cur_year_tick < growth.timing_1 or
               df.global.cur_year_tick > growth.timing_2) then
-            dfhack.println ("Found " .. v.id .. " but it's not ripe")
+            dfhack.color (COLOR_YELLOW)
+            dfhack.println ("Found " .. v.id .. " but it's not ripe (" .. tostring (#candidates) .. ")")
+            dfhack.color (COLOR_RESET)
             designated = false
             break
           end
@@ -160,7 +223,9 @@ function getone ()
                   
           for l = 1, last do
             if candidates [l] [2] then
-              dfhack.println ("Already designated " .. v.id, candidates [l] [1].pos.x, candidates [l] [1].pos.y, candidates [l] [1].pos.z)
+              if verbose then
+                dfhack.println ("Already designated " .. v.id, candidates [l] [1].pos.x, candidates [l] [1].pos.y, candidates [l] [1].pos.z)
+              end
               
             else
               local cur = dfhack.maps.getTileBlock (candidates [l] [1].pos)
@@ -169,7 +234,11 @@ function getone ()
             
               cur.designation [x] [y].dig = df.tile_dig_designation.Default
               cur.flags.designated = true
-              dfhack.println ("Designated " .. v.id, candidates [l] [1].pos.x, candidates [l] [1].pos.y, candidates [l] [1].pos.z)
+              count = count + 1
+              
+              if verbose then
+                dfhack.println ("Designated " .. v.id, candidates [l] [1].pos.x, candidates [l] [1].pos.y, candidates [l] [1].pos.z)
+              end
             end
           end
         end
@@ -177,17 +246,23 @@ function getone ()
       
       if not skip then
         if #candidates == 0 then
+          dfhack.color (COLOR_LIGHTRED)
           dfhack.println ("Failed to find any " .. v.id)
+          dfhack.color (COLOR_RESET)
           
         elseif not designated then
           --  Not ripe
         
         elseif #candidates < number_to_get - seeds then
-          dfhack.println ("Failed to find sufficient " .. v.id)
+          dfhack.color (COLOR_LIGHTCYAN)
+          dfhack.println ("Failed to find sufficient " .. v.id .. " (" .. tostring (#candidates) .. ")")
+          dfhack.color (COLOR_RESET)
         end
       end
     end
   end
+  
+  dfhack.println ("Designated " ..  tostring (count) .. " plants for gathering")
 end
 
-getone()
+getone ()
